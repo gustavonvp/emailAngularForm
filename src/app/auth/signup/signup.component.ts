49d5e373e4b9e9c3f07f16e333b97a917903f03b
Gustavonvp <gustavonvp@gmail.com>
2022-01-03 18:25:26 -0300

    form with interpolation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+
+  authForm = new FormGroup({
+    
+    username: new FormControl('', [ Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z9-9]+$/)]),
+      
+    password: new FormControl(''),
+    passwordConfiguration: new FormControl(''),
+  })
 
   constructor() { }
 
