591e7176a37d178cca1337378838f73a9edad034
Gustavonvp <gustavonvp@gmail.com>
2022-01-03 21:11:56 -0300

    puting some synchronous validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
 
 @Component({
   selector: 'app-signup',
@@ -14,7 +15,7 @@ export class SignupComponent implements OnInit {
     username: new FormControl('', [ Validators.required,
       Validators.minLength(3),
       Validators.maxLength(20),
-      Validators.pattern(/^[a-z9-9]+$/)]),
+      Validators.pattern(/^[a-z9-9]+$/)], [this.uniqueUsername.validate]),
       
     password: new FormControl('', [
       Validators.required,
@@ -29,7 +30,10 @@ export class SignupComponent implements OnInit {
     
   }, {validators: [this.matchPassword.validate]})
 
-  constructor(private matchPassword: MatchPassword) { }
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername, 
+    ) { }
 
   ngOnInit() {
   }
