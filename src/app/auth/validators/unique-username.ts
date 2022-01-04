774f8e20bf36aaed485304ef49226937712b2b48
Gustavonvp <gustavonvp@gmail.com>
2022-01-03 21:36:50 -0300

    setting no connection fetch option

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
-
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
@@ -12,7 +13,20 @@ export class UniqueUsername implements AsyncValidator {
             const { value } = control;
             return this.http.post<any>('https://api.angular-email.com/auth/username', {
                 username: value
-            }
-            )
+            })
+            .pipe(
+                map(value => {
+                    if(value.available){
+                        return null;
+                    }    
+                }),
+            catchError((err) => {
+                console.log(err);
+                if(err.message.username) {
+                    return of({nonUniqueUsername: true})
+                }
+                return of({ noConnection: true });
+            })
+            );
         }
 }
